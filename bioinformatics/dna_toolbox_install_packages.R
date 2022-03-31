4dcd06f6f6edf66abfeda399a7de84964d2189a2
Todor Gitchev <todor.gitchev@gmail.com>
2022-03-31 13:56:14 +0200

    minor comment update

--- bioinformatics/dna_toolbox_install_packages.R
+++ bioinformatics/dna_toolbox_install_packages.R
@@ -31,4 +31,4 @@ library(CNAclinic)
 # ichorCNA
 install_github("broadinstitute/ichorCNA")
 
-# TODO install_jypiter_dependencies(packages = c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'uuid', 'digest'))
+# IF_NEEDED: install_jypiter_dependencies(packages = c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'uuid', 'digest'))
