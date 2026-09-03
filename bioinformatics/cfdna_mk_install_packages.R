9b6e78f4e8ca68963ab0b4689321ffaca9a7915a
Todor Gitchev <todor.gitchev@uzh.ch>
2026-09-03 11:35:08 +0200

    try to resolve R dependencies issues

--- bioinformatics/cfdna_mk_install_packages.R
+++ bioinformatics/cfdna_mk_install_packages.R
@@ -1,6 +1,12 @@
 # Title     : Install R package dependencies
 # Objective : prepare environment to run the R scripts in this project
 
+# R -f continues after errors by default; fail the image build instead.
+options(error = function() {
+  traceback(2)
+  quit(save = "no", status = 1)
+})
+
 install_dependencies <- function(packages) for (package in packages) if (!requireNamespace(package, quietly = TRUE)) install.packages(package, quietly = TRUE) else print(paste("Package", package, " already installed."))
 
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager", quietly = TRUE)
@@ -8,7 +14,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocMana
 install_Bioc_dependencies <- function(packages) {
   for (package in packages) {
      if (!requireNamespace(package, quietly = TRUE)) {
-       BiocManager::install(package, quietly = TRUE)
+       BiocManager::install(package, quietly = TRUE, ask = FALSE, update = FALSE)
      } else {
        print(paste("Bioconductor package", package, " already installed."))
      }
@@ -26,16 +32,43 @@ install_jypiter_dependencies <- function (packages) {
 R.Version()
 # update.packages(ask = FALSE)
 
-#install_Bioc_dependencies(packages = c("org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg19.knownGene",
-#                                       "TxDb.Hsapiens.UCSC.hg38.knownGene", "QDNAseq.hg19", "QDNAseq.hg38", "BSgenome.Hsapiens.UCSC.hg38",
-#                                       "BSgenome.Hsapiens.UCSC.hg19"))
+install_dependencies(packages = c(
+  "ggplot2", "optparse", "this.path", "remotes",
+  "dplyr", "data.table", "scales", "R.utils", "matrixStats", "PSCBS"
+))
 
-install_dependencies(packages = c("ggplot2", "optparse", "this.path", "remotes"))
+# CNAclinic Imports plus ichorCNA Bioconductor deps still on the current release
+install_Bioc_dependencies(packages = c(
+  "HMMcopy", "GenomeInfoDb", "GenomicRanges", "IRanges",
+  "Rsamtools", "QDNAseq", "QDNAseq.hg19",
+  "DNAcopy", "CGHbase", "CGHcall", "Biobase", "annotate",
+  "org.Hs.eg.db",
+  "TxDb.Hsapiens.UCSC.hg19.knownGene",
+  "TxDb.Hsapiens.UCSC.hg38.knownGene"
+))
 
-install_Bioc_dependencies(packages = c("HMMcopy", "GenomeInfoDb", "GenomicRanges"))
+# copynumber was removed from Bioconductor 3.18; last release is 3.16.
+# remotes cannot resolve it from current repos, so CNAclinic install was a no-op.
+if (!requireNamespace("copynumber", quietly = TRUE)) {
+  install.packages(
+    "https://bioconductor.org/packages/3.16/bioc/src/contrib/copynumber_1.38.0.tar.gz",
+    repos = NULL,
+    type = "source"
+  )
+}
 
-remotes::install_github("sdchandra/CNAclinic", build_vignettes = FALSE, dependencies = TRUE)
+remotes::install_github(
+  "sdchandra/CNAclinic",
+  build_vignettes = FALSE,
+  dependencies = TRUE,
+  upgrade = "never"
+)
 
-remotes::install_github("broadinstitute/ichorCNA")
+remotes::install_github("broadinstitute/ichorCNA", upgrade = "never")
+
+stopifnot(
+  requireNamespace("ichorCNA", quietly = TRUE),
+  requireNamespace("CNAclinic", quietly = TRUE)
+)
 
 # IF_NEEDED: install_jypiter_dependencies(packages = c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'uuid', 'digest'))
